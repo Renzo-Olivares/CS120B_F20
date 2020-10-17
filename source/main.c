989babaf4f94de16a1decb3cf3835036ac47a7db
Renzo <rmolivares@renzo-olivares.dev>
2020-10-16 21:45:31 -0700

    test

--- source/main.c
+++ source/main.c
@@ -63,42 +63,7 @@ int main(void) {
     state = START;
 
     while(1){
-        switch(state){
-            case START:
-                state = OFF;
-                break;
-            case OFF:
-                if(!PINA0 || PINA1){
-                    state = OFF;
-                }
-                if(PINA0 && !PINA1){
-                    state = ON;
-                }
-                break;
-            case ON:
-                if(PINA0 && !PINA1){
-                    state = ON;
-                }
-                if(!PINA0 || PINA1){
-                    state = OFF;
-                }
-                break;
-            default:
-                break;
-        }
-
-        switch(state){
-            case START:
-                break;
-            case OFF:
-                PORTB = 0;
-                break;
-            case ON:
-                PORTB = 0x01;
-                break;
-            default:
-                break;
-        }
+        Tick();
     }
 
     return 1;

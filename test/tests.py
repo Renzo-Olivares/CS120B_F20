cc870c6e7bbaaad883ee75d0f3e47d8ee426c9b1
Renzo <rmolivares@renzo-olivares.dev>
2020-10-16 22:55:26 -0700

    test

--- test/tests.py
+++ test/tests.py
@@ -43,5 +43,5 @@ tests = [
 # Optionally you can add a set of "watch" variables these need to be global or static and may need
 # to be scoped at the function level (for static variables) if there are naming conflicts. The 
 # variables listed here will display everytime you hit (and stop at) a breakpoint
-watch = ['Tick::state','PORTB']
+# watch = ['main::state','PORTB']
 

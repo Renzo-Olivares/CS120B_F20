987da4dd1e5260686976a61d0d0a8bfe563adbd2
Renzo <rmolivares@renzo-olivares.dev>
2020-10-16 21:40:02 -0700

    test

--- source/main.c
+++ source/main.c
@@ -21,7 +21,6 @@ void Tick(){
             state = OFF;
             break;
         case OFF:
-            printf("hello world");
             if(!PINA0 || PINA1){
                 state = OFF;
             }
@@ -64,7 +63,42 @@ int main(void) {
     state = START;
 
     while(1){
-        Tick();
+        switch(state){
+            case START:
+                state = OFF;
+                break;
+            case OFF:
+                if(!PINA0 || PINA1){
+                    state = OFF;
+                }
+                if(PINA0 && !PINA1){
+                    state = ON;
+                }
+                break;
+            case ON:
+                if(PINA0 && !PINA1){
+                    state = ON;
+                }
+                if(!PINA0 || PINA1){
+                    state = OFF;
+                }
+                break;
+            default:
+                break;
+        }
+
+        switch(state){
+            case START:
+                break;
+            case OFF:
+                PORTB = 0;
+                break;
+            case ON:
+                PORTB = 0x01;
+                break;
+            default:
+                break;
+        }
     }
 
     return 1;

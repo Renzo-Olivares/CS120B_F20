64cccd292f833b2846fa476940e1b07e897cad4c
Renzo <rmolivares@renzo-olivares.dev>
2020-10-20 11:33:57 -0700

    test fix for lab3 part 2

--- source/main.c
+++ source/main.c
@@ -1,10 +1,11 @@
 /*	Author: roliv006
  *      Partner(s) Name: 
  *	Lab Section: 22
- *	Assignment: Lab #3  Exercise #3
- *	Exercise Description: In addition to the above, PA4 is 1 if a key is in the ignition, PA5 is one if a driver is seated, and PA6
- *  is 1 if the driver's seatbelt is fastened. PC7 should light a "Fasten seatbelt" icon if a key is in the ignition, there driver is
- *  seated, but the belt is not fastened.
+ *	Assignment: Lab #3  Exercise #2
+ *	Exercise Description: A car has a fuel-level sensor that sets PA3..PA0 to a value between 0 (empty) and 15 (full). A series
+ *  of LEDs connected to PC%..PC0 should light to graphically indicate the fuel level. If the fuel level is 1 or 2, PC5 lights.
+ *  If the level is 3 or 4, PC5 and PC4 light. Level 5-6 PC5..PC3. 7-9 PC5..PC2. 10-12 lights PC5..PC1. 13-15 lights PC5..PC0.
+ *  Also, PC6 connects to a "Low fuel" icon, which should light if the level is 4 or less. 
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -25,51 +26,47 @@ int main(void) {
         unsigned char pinA1 = PINA & 0x02;
         unsigned char pinA2 = PINA & 0x04;
         unsigned char pinA3 = PINA & 0x08;
-        unsigned char pinA4 = PINA & 0x10;
-        unsigned char pinA5 = PINA & 0x20;
-        unsigned char pinA6 = PINA & 0x40;
 
         unsigned char fuellevel = pinA0 + pinA1 + pinA2 + pinA3;
-        unsigned char fastenwarning = (pinA4 && pinA5) && !pinA6;
 
         switch(fuellevel){
             case 0x00:
-                PORTC = fastenwarning? 0xC0: 0x40;
+                PORTC = 0x40;
                 break;
             case 0x01:
             case 0x02:
                 //light PC5
                 //light low fuel PC6
-                PORTC = fastenwarning? 0xE0: 0x60;
+                PORTC = 0x60;
                 break;
             case 0x03:
             case 0x04:
                 //light PC5 and PC4
                 //light low fuel PC6
-                PORTC = fastenwarning? 0xF0:0x70;
+                PORTC = 0x70;
                 break;
             case 0x05:
             case 0x06:
                 //light PC5..PC3
-                PORTC = fastenwarning? 0xB8:0x38;
+                PORTC = 0x38;
                 break;
             case 0x07:
             case 0x08:
             case 0x09:
                 //light PC5..PC2
-                PORTC = fastenwarning? 0xBC:0x3C;
+                PORTC = 0x3C;
                 break;
             case 0x0A:
             case 0x0B:
             case 0x0C:
                 //light PC5..PC1
-                PORTC = fastenwarning? 0xBE:0x3E;
+                PORTC = 0x3E;
                 break;
             case 0x0D:
             case 0x0E:
             case 0x0F:
                 //light PC5..PC0
-                PORTC = fastenwarning? 0xBF:0x3F;
+                PORTC = 0x3F;
                 break;
             default:
                 break;

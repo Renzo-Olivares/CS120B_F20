bfcd92038095b4a9d564e6d0962902f73aacc91c
Renzo <rmolivares@renzo-olivares.dev>
2020-10-20 00:22:35 -0700

    test

--- source/main.c
+++ source/main.c
@@ -23,22 +23,22 @@ int main(void) {
     while (1) {
         unsigned char pinA0 = PINA & 0x01;
         unsigned char pinA1 = (PINA & 0x02) >> 1;
-        unsigned char pinA2 = PINA & 0x04 >> 2;
-        unsigned char pinA3 = PINA & 0x08 >> 3;
-        unsigned char pinA4 = PINA & 0x10 >> 4;
-        unsigned char pinA5 = PINA & 0x20 >> 5;
-        unsigned char pinA6 = PINA & 0x40 >> 6;
-        unsigned char pinA7 = PINA & 0x80 >> 7;
-        // unsigned char pinB0 = PINB & 0x01;
-        // unsigned char pinB1 = PINB & 0x02 >> 1;
-        // unsigned char pinB2 = PINB & 0x04 >> 2;
-        // unsigned char pinB3 = PINB & 0x08 >> 3;
-        // unsigned char pinB4 = PINB & 0x10 >> 4;
-        // unsigned char pinB5 = PINB & 0x20 >> 5;
-        // unsigned char pinB6 = PINB & 0x40 >> 6;
-        // unsigned char pinB7 = PINB & 0x80 >> 7;
-        // unsigned char total = pinA0 + pinA1 + pinA2 + pinA3 + pinA4 + pinA5 + pinA6 + pinA7 + pinB0 + pinB1 + pinB2 + pinB3 + pinB4 + pinB5 + pinB6 + pinB7;
-        unsigned char total = pinA0 + pinA1 + pinA2 + pinA3 + pinA4 + pinA5 + pinA6 + pinA7;
+        unsigned char pinA2 = (PINA & 0x04) >> 2;
+        unsigned char pinA3 = (PINA & 0x08) >> 3;
+        unsigned char pinA4 = (PINA & 0x10) >> 4;
+        unsigned char pinA5 = (PINA & 0x20) >> 5;
+        unsigned char pinA6 = (PINA & 0x40) >> 6;
+        unsigned char pinA7 = (PINA & 0x80) >> 7;
+        unsigned char pinB0 = PINB & 0x01;
+        unsigned char pinB1 = (PINB & 0x02) >> 1;
+        unsigned char pinB2 = (PINB & 0x04) >> 2;
+        unsigned char pinB3 = (PINB & 0x08) >> 3;
+        unsigned char pinB4 = (PINB & 0x10) >> 4;
+        unsigned char pinB5 = (PINB & 0x20) >> 5;
+        unsigned char pinB6 = (PINB & 0x40) >> 6;
+        unsigned char pinB7 = (PINB & 0x80) >> 7;
+        unsigned char total = pinA0 + pinA1 + pinA2 + pinA3 + pinA4 + pinA5 + pinA6 + pinA7 + pinB0 + pinB1 + pinB2 + pinB3 + pinB4 + pinB5 + pinB6 + pinB7;
+
         PORTC = total;
     }
 

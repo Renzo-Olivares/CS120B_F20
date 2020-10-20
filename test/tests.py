7542e3643cf7302854830e12b4e3390b08a183e4
Renzo <rmolivares@renzo-olivares.dev>
2020-10-20 00:12:01 -0700

    test

--- test/tests.py
+++ test/tests.py
@@ -14,8 +14,17 @@
 # An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
 # that they are not ran in isolation but in the order shown and the state of the device is not reset or 
 # altered in between executions (unless preconditions are used).
-tests = [ {'description': 'This test will run first.',
-    'steps': [ {'inputs': [('PINA',0x01),('PINB', 0x00)], 'iterations': 1 } ],
+tests = [ 
+    {'description': 'This test will run first.',
+    'steps': 
+    [ 
+        {
+            'inputs': [('PINA',0x01)], 'iterations': 1 
+        },
+        {
+            'inputs': [('PINB',0x00)], 'iterations': 1 
+        } 
+    ],
     'expected': [('PORTC',0x01)],
     },
     # {'description': 'This test will run second.',

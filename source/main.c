7084fc6573da3b877c9685af2bf9363e8f931b23
Renzo <rmolivares@renzo-olivares.dev>
2020-10-19 20:58:31 -0700

    Upload part2 files for testing

--- source/main.c
+++ source/main.c
@@ -1,10 +1,12 @@
 /*	Author: roliv006
  *  Partner(s) Name: 
  *	Lab Section: 22
- *	Assignment: Lab #2  Exercise #1
- *	Exercise Description: Garage open at night -- A garage door sensor connects to PA0 (1 means door open), and a light sensor
- *  connects to PA1(1 means light is sensed). Write a program that illuminates an LED connected to PB0(1 means illuminate) if the
- *  garage door is open at night.
+ *	Assignment: Lab #2  Exercise #2
+ *	Exercise Description: Port A's pins 3 to 0, each connect to a parking space sensor, 1 meaning
+ *  a car is parked in the space, of a four space parking lot. Write a program" that outputs in
+ *  binary on Port C the number of available spaces (Hint: declare a variable "unsigned char cntavail;"
+ *  you can assign a number to a port as follows: PORTC = cntavail;).
+ *  
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -14,54 +16,94 @@
 #include "simAVRHeader.h"
 #endif
 
-enum States {START, OFF, ON}state;
+enum States{START, COUNT, ONE, TWO, THREE, FOUR, OUTPUT}state;
+unsigned char availcnt = 0x00;
+unsigned char pastSensor = 0x00;
+
 void Tick(){
-    unsigned char pinA0 = PINA & 0x01;
-    unsigned char pinA1 = PINA & 0x02;
-
-    switch(state){
-        case START:
-            state = OFF;
-            break;
-        case OFF:
-            if(!pinA0 || pinA1){
-                state = OFF;
-            }
-            if(pinA0 && !pinA1){
-                state = ON;
-            }
-            break;
-        case ON:
-            if(pinA0 && !pinA1){
-                state = ON;
-            }
-            if(!pinA0 || pinA1){
-                state = OFF;
-            }
-            break;
-        default:
-            break;
-    }
-
-    switch(state){
-        case START:
-            break;
-        case OFF:
-            PORTB = 0x00;
-            break;
-        case ON:
-            PORTB = 0x01;
-            break;
-        default:
-            break;
-    }
+   switch(state){
+      case START:
+         state = COUNT;
+         printf("start transition\n");
+         break;
+      case COUNT:
+         state = !A0 && !A1 && !A2 && !A3? OUTPUT: ONE;
+         printf("count transition\n");
+         break;
+      case ONE:
+         state = A1 || A2 || A3? TWO: OUTPUT;
+         printf("one transition\n");
+         break;
+      case TWO:
+         state = A1 || A2? THREE: OUTPUT;
+         printf("two transition\n");
+         break;
+      case THREE:
+         state = A2? FOUR: OUTPUT;
+         printf("three transition\n");
+         break;
+      case FOUR:
+         state = OUTPUT;
+         printf("four transition\n");
+         break;
+      case OUTPUT:
+         printf("out transition\n");
+         state = pastSensor != (A0 + A1 + A2 + A3)? COUNT: OUTPUT;
+         break;
+      default:
+         printf("error transition\n");
+         break;
+   }
+   switch(state){
+      case START:
+         printf("start state\n");
+         break;
+      case COUNT:
+         B = 0x00;
+         availcnt = 0x00;
+         pastSensor = 0x00;
+         printf("count state\n");
+         break;
+      case ONE:
+         if(A0){
+            availcnt++;
+         }
+         printf("one state\n");
+         break;
+      case TWO:
+         if(A1){
+            availcnt++;
+         }
+         printf("two sttae\n");
+         break;
+      case THREE:
+         if(A2){
+            availcnt++;
+         }
+         printf("three state\n");
+         break;
+      case FOUR:
+         if(A3){
+            availcnt++;
+         }
+         pastSensor = A0 + A1 + A2 + A3;
+         printf("four state\n");
+         break;
+      case OUTPUT:
+         PORTC = availcnt;
+         printf("out state\n");
+         break;
+      default:
+         printf("error state\n");
+         break;
+   }
 }
 
 int main(void) {
     DDRA = 0x00;
     PORTA = 0xFF;
-    DDRB = 0xFF;
-    PORTB = 0x00;
+    DDRC = 0xFF;
+    PORTC = 0x00;
 
     state = START;
 

033ce7f7cb000d87e9ab13b3949cacc029c3f7a7
Renzo <rmolivares@renzo-olivares.dev>
2020-10-25 23:43:48 -0700

    Upload test files for lab4 part 2

--- source/main.c
+++ source/main.c
@@ -1,10 +1,10 @@
 /*	Author: roliv006
  *  Partner(s) Name: 
  *	Lab Section: 22
- *	Assignment: Lab #4  Exercise #1
- *	Exercise Description: PB0 and PB1 each connect to an LED, and PB0's LED is initially on. Pressing a button connected to PA0 turns
- *  off PB0's LED and turns on PB1's LED, staying that way after button release. Pressing the button again turns off PB1's LED and turns
- *  on PB0's LED.
+ *	Assignment: Lab #4  Exercise #2
+ *	Exercise Description: Buttons are connected to a PA0 and PA1. Output for PORTC is initially 7. Pressing PA0 increments PORTC once
+ *  (stopping at 9). Pressing PA1 decrements PORTC once (stopping at 0). If both buttons are depressed (even if not initially simultaneously)
+ *  , PORTC resets to 0.
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -14,27 +14,53 @@
 #include "simAVRHeader.h"
 #endif
 
-enum States{START, OFF_RELEASE, ON_PRESS, ON_RELEASE, OFF_PRESS}state;
+enum States{START, DECREMENT_RELEASE, INCREMENT_PRESS, INCREMENT_RELEASE, DECREMENT_PRESS}state;
 
 void Tick(){
     unsigned char pinA0 = PINA && 0x01;
+    unsigned char pinA1 = PINA && 0x02;
+    unsigned char cnt = 0x07;
 
     switch(state){
         case START:
-            PORTB = 0x01;
-            state = OFF_RELEASE;
+            PORTC = cnt;
+            state = DECREMENT_RELEASE;
             break;
-        case OFF_RELEASE:
-            state = pinA0? ON_PRESS : OFF_RELEASE;
+        case DECREMENT_RELEASE:
+            if(pinA0){
+                state = INCREMENT_PRESS;
+            }else if(pinA1){
+                state = DECREMENT_PRESS;
+            }else{
+                state = DECREMENT_RELEASE;
+            }
             break;
-        case ON_PRESS:
-            state = pinA0? ON_PRESS : ON_RELEASE;
+        case INCREMENT_PRESS:
+            if(pinA0){
+                state = INCREMENT_PRESS;
+            }else if(pinA1){
+                state = DECREMENT_PRESS;
+            }else{
+                state = INCREMENT_PRESS;
+            }
             break;
-        case ON_RELEASE:
-            state = pinA0? OFF_PRESS: ON_RELEASE;
+        case INCREMENT_RELEASE:
+            if(pinA0){
+                state = INCREMENT_PRESS;
+            }else if(pinA1){
+                state = DECREMENT_PRESS;
+            }else{
+                state = INCREMENT_RELEASE;
+            }
             break;
-        case OFF_PRESS:
-            state = pinA0? OFF_PRESS : OFF_RELEASE;
+        case DECREMENT_PRESS:
+            if(pinA0){
+                state = INCREMENT_PRESS;
+            }else if(pinA1){
+                state = DECREMENT_PRESS;
+            }else{
+                state = DECREMENT_PRESS;
+            }
             break;
         default:
             break;
@@ -43,15 +69,25 @@ void Tick(){
     switch(state){
         case START:
             break;
-        case OFF_RELEASE:
+        case DECREMENT_RELEASE:
+            if(!pinA0 && !pinA1){
+                PORTC = 0x00;
+            }
             break;
-        case ON_PRESS:
-            PORTB = 0x02;
+        case INCREMENT_PRESS:
+            if(cnt != 9){
+                cnt++;
+            }
             break;
-        case ON_RELEASE:
+        case INCREMENT_RELEASE:
+            if(!pinA0 && !pinA1){
+                PORTC = 0x00;
+            }
             break;
-        case OFF_PRESS:
-            PORTB = 0x01;
+        case DECREMENT_PRESS:
+            if(cnt != 0){
+                cnt--;
+            }
             break;
         default:
             break;
@@ -62,8 +98,8 @@ int main(void) {
     /* Insert DDR and PORT initializations */
     DDRA = 0x00;
     PORTA = 0xFF;
-    DDRB = 0xFF; // Configure port B's 8 pins as outputs
-    PORTB = 0x00; // Initialize PORTB output to 0's
+    DDRC = 0xFF; // Configure port C's 8 pins as outputs
+    PORTC = 0x00; // Initialize PORTC output to 0's
 
     state = START;
     

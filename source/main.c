9bd3603835642fee744153d07d3cebd17066c0a6
Renzo <rmolivares@renzo-olivares.dev>
2020-11-08 16:06:53 -0800

    fix typos add comments

--- source/main.c
+++ source/main.c
@@ -8,71 +8,75 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
-#include <avr/interrupt.h>
+// #include <avr/interrupt.h>
+#include "timer.h"
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
-// TimerISR() sets this to 1. C programmer should clear to 0.
-volatile unsigned char TimerFlag = 0;
+// // TimerISR() sets this to 1. C programmer should clear to 0.
+// volatile unsigned char TimerFlag = 0;
 
-// Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
-unsigned long _avr_timer_M = 1; // Start count from herer, down to 0. Default 1ms.
-unsigned long _avr_time_cntcurr = 0; //Current internal count of 1ms ticks
+// // Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
+// unsigned long _avr_timer_M = 1; // Start count from herer, down to 0. Default 1ms.
+// unsigned long _avr_time_cntcurr = 0; //Current internal count of 1ms ticks
 
-void TimerOn() {
-    // AVR timer/counter controller register TCCR1
-    TCCR1B = 0x0B; // bit3 = 0: CTC mode (clear timer on compare)
-                   // bit2bit1bit0=011: pre-scaler /64
-                   // 00001011: 0x0B
-                   // SO, 8 MHz clock or 8,000,000 /64 = 125,000 ticks/s
-                   // Thus, TCNT1 register will count at 125,000 ticks/s
+// void TimerOn() {
+//     // AVR timer/counter controller register TCCR1
+//     TCCR1B = 0x0B; // bit3 = 0: CTC mode (clear timer on compare)
+//                    // bit2bit1bit0=011: pre-scaler /64
+//                    // 00001011: 0x0B
+//                    // SO, 8 MHz clock or 8,000,000 /64 = 125,000 ticks/s
+//                    // Thus, TCNT1 register will count at 125,000 ticks/s
 
-    OCR1A = 125;
+//     OCR1A = 125;
 
-    TIMSK1 = 0x02;
+//     TIMSK1 = 0x02;
 
-    TCNT1 = 0;
+//     TCNT1 = 0;
 
-    _avr_time_cntcurr = _avr_timer_M;
+//     _avr_time_cntcurr = _avr_timer_M;
 
-    SREG |= 0x80;
-}
+//     SREG |= 0x80;
+// }
 
-void TimerOff() {
-    TCCR1B = 0x00;
-}
+// void TimerOff() {
+//     TCCR1B = 0x00;
+// }
 
-void TimerISR() {
-    TimerFlag = 1;
-}
+// void TimerISR() {
+//     TimerFlag = 1;
+// }
 
-ISR(TIMER1_COMPA_vect) {
-    _avr_timer_cntcurr--;
-    if (_avr_timer_cntcurr == 0){
-        TimerISR();
-        _avr_timer_cntcurr = _avr_timer_M;
-    } 
-}
+// ISR(TIMER1_COMPA_vect) {
+//     _avr_timer_cntcurr--;
+//     if (_avr_timer_cntcurr == 0){
+//         TimerISR();
+//         _avr_timer_cntcurr = _avr_timer_M;
+//     } 
+// }
 
-void TimerSet(unsigned long M) {
-    _avr_timer_M = M;
-    _avr_timer_cntcurr = _avr_timer_M;
-}
+// void TimerSet(unsigned long M) {
+//     _avr_timer_M = M;
+//     _avr_timer_cntcurr = _avr_timer_M;
+// }
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-    DDRB = 0xFF;
-    PORTB = 0x00;
+    DDRB = 0xFF; // Set port B to output
+    PORTB = 0x00; // Init port B to 0s
     TimerSet(1000);
     TimerOn();
     unsigned char tmpB = 0x00;
     /* Insert your solution below */
     while (1) {
-        tmpb = -tmpB;
+        // User code (i.e. syncSM calls)
+        tmpB = ~tmpB; //Toggle PORTB; Temporary, bad programming style
         PORTB = tmpB;
-        while(!TimerFlag);
+        while(!TimerFlag); // Wait 1 SEC
         TimerFlag = 0;
+        // Note: For the above a better style would use a syncSm with TickSM()
+        // This example just illsutrates the use of the ISR and flag
     }
     return 1;
 }

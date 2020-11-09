9106e4b2790e7f5a9e6b5f96fc7915b86bb2b185
Renzo <rmolivares@renzo-olivares.dev>
2020-11-08 17:03:17 -0800

    Upload final test for lab 6 exercise 1

--- source/main.c
+++ source/main.c
@@ -1,8 +1,14 @@
-/*	Author: renzo
+/*	Author: roliv006
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
+ *	Lab Section: 22
+ *	Assignment: Lab #6  Exercise #1
+ *	Exercise Description: Create a synchSM to blink three LEDs connected to PB0, PB1, and PB2
+ *  in sequence, 1 second each. Implement that synchSM in C using the method defined in class. 
+ *  In addition to demoing your program, you will need to show that your code adheres entirely 
+ *  to the method with no variations.
+ * 
+ *  To clarify and match the video, the lights should turn on in the following sequence:
+ *  PB0,PB1,PB2,PB0,PB1,PB2,PB0,PB1,PB2…
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -13,18 +19,51 @@
 #include "simAVRHeader.h"
 #endif
 
+enum States{START, INIT, NEXT}state;
+unsigned char out;
+
+void Tick() {
+    // State transition
+    switch(state){
+        case START:
+            state = INIT;
+            break;
+        case INIT:
+            state = NEXT;
+            break;
+        case NEXT:
+            state = NEXT;
+            break;
+        default:
+            break;
+    }
+
+    // State actions
+    switch(state){
+        case INIT:
+            PORTB = out = 0x01;
+            break;
+        case NEXT:
+            out = (out == 0x04) ? 0x01 : out << 1;
+            PORTB = out;
+            break;
+        default:
+            break;
+    }
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
     DDRB = 0xFF; // Set port B to output
     PORTB = 0x00; // Init port B to 0s
+    state = START;
     TimerSet(1000);
     TimerOn();
-    unsigned char tmpB = 0x00;
+
     /* Insert your solution below */
     while (1) {
         // User code (i.e. syncSM calls)
-        tmpB = ~tmpB; //Toggle PORTB; Temporary, bad programming style
-        PORTB = tmpB;
+        Tick();
         while(!TimerFlag); // Wait 1 SEC
         TimerFlag = 0;
         // Note: For the above a better style would use a syncSm with TickSM()
--- test/tests.py
+++ test/tests.py
@@ -14,21 +14,34 @@
 # An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
 # that they are not ran in isolation but in the order shown and the state of the device is not reset or 
 # altered in between executions (unless preconditions are used).
-tests = [ {'description': 'This test will run first.',
-    'steps': [ {'inputs': [('PINA',<val>)], 'iterations': 1 } ],
-    'expected': [('PORT',<val>)],
+tests = [ 
+    {'description': 'First LED',
+    'steps': [ {'inputs': [], 'time': 1000 } ],
+    'expected': [('PORTB',0x01)],
     },
-    {'description': 'This test will run second.',
-    'steps': [ {'inputs': [('PIN', <val>)],'iterations': 1}, # Set PIN to val then run one iteration
-        {'inputs': [('PIN',<val>)], 'time': 300 }, # Set PIN to val then run 300 ms
-        {'inputs': [('PIN',<val>)], 'iterations': 1, 'expected': [('PORT',<val>)]}, 
-        {'inputs': [('PIN',<val>)], 'time': 600}, ],
-    'expected': [('PORT',<val>)],
+    {'description': 'Second LED',
+    'steps': [ {'inputs': [], 'time': 1000 } ],
+    'expected': [('PORTB',0x02)],
     },
+    {'description': 'Third LED',
+    'steps': [ {'inputs': [], 'time': 1000 } ],
+    'expected': [('PORTB',0x04)],
+    },
+    {'description': 'First LED Again',
+    'steps': [ {'inputs': [], 'time': 1000 } ],
+    'expected': [('PORTB',0x01)],
+    },
+    # {'description': 'This test will run second.',
+    # 'steps': [ {'inputs': [('PIN', <val>)],'iterations': 1}, # Set PIN to val then run one iteration
+    #     {'inputs': [('PIN',<val>)], 'time': 300 }, # Set PIN to val then run 300 ms
+    #     {'inputs': [('PIN',<val>)], 'iterations': 1, 'expected': [('PORT',<val>)]}, 
+    #     {'inputs': [('PIN',<val>)], 'time': 600}, ],
+    # 'expected': [('PORT',<val>)],
+    # },
     ]
 
 # Optionally you can add a set of "watch" variables these need to be global or static and may need
 # to be scoped at the function level (for static variables) if there are naming conflicts. The 
 # variables listed here will display everytime you hit (and stop at) a breakpoint
-watch = ['<function>::<static-var>','PORTB']
+watch = ['Tick::out','PORTB']
 
